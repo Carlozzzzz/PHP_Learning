9bb9f8549b136ff738e33d75e7b8a2f3982a8ec0
carlozzzzz <carlosromulomaralit@gmail.com>
2023-10-20 15:22:31 +0800

    upload php project

---
+++ __php_project/pages/gallery.php
@@ -0,0 +1,11 @@
+<?php include "../templates/header.php";?>
+
+<!-- Content -->
+<section id="main-section">
+    <div class="container pt-5">
+        <h3>This is Gallery</h3>
+        <p>soon...</p>
+    </div>
+</section>
+
+<?php include "../templates/footer.php";?>
