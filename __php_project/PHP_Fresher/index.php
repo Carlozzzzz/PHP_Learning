54e58eff04ac4c74f09bd8b79bbd1dba5d69ed91
carlozzzzz <carlosromulomaralit@gmail.com>
2024-03-28 08:26:25 +0800

    PHP_Fresher : create initial files

---
+++ __php_project/PHP_Fresher/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/homepage.css">
+    <title>Simple CRUD</title>
+</head>
+<body>
+    <div class="auth-msg">
+        <p>This page requried <b>authentication</b> otherwise this will redirect to <b>home page</b>.</p>
+    </div>
+</body>
+</html>
---
+++ __php_project/PHP_Fresher/view/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/homepage.css">
+
+    <title>Login</title>
+</head>
+<body>
+    <form action="#">
+        <div>
+            <label for="usernameTxt">Username:</label>
+            <input type="text" id="usernameTxt">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="passwordTxt">
+        </div>
+        <div>
+            <button>Login</button>
+            <button>Register</button>
+        </div>
+    </form>
+</body>
+</html>
